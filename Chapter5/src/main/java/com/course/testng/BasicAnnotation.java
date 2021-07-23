bbae16fd7e54b8cbd0c590c11b12b94b115ecad7
wb-whj595946 <wb-whj595946@antfin.com>
2021-07-23 10:28:06 +0800

    第一个用例

---
+++ Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
@@ -0,0 +1,11 @@
+package com.course.testng;
+
+import org.testng.annotations.Test;
+
+public class BasicAnnotation {
+
+    @Test
+    public void test1(){
+        System.out.println("²âÊÔÓÃÀı1");
+    }
+}
